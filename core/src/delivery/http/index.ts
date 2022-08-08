75e2f0df338e4f43cccee5b669c4cbf2c216f668
Novik-Artem <artemgithab@gmail.com>
2022-08-08 17:16:45 +0300

    added random quote

--- core/src/delivery/http/index.ts
+++ core/src/delivery/http/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import routerCharacters from "./characters";
+import routerQuotes from "./quotes";
 
 const app: express.Application = express();
 
 app.use(express.json());
 app.use("/api", routerCharacters);
-
+app.use("/api", routerQuotes);
 export default app;
